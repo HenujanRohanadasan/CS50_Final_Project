feba3e97081b1de659b8de0d683fc06a855f047c
Henujan <henujan2k6@gmail.com>
2026-02-09 21:16:02 +0530

    adiing regiter login and about templates and setting up

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+import os
+
+from flask import Flask, render_template, flash
 
 app = Flask(__name__)
+app.secret_key = str(os.environ.get('SECRET_KEY'))
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
