1ba5c43e19f191c453953c1690ec77bef3186c06
Henujan <henujan2k6@gmail.com>
2026-03-15 22:34:03 +0530

    adding user model for data base

---
+++ models.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
