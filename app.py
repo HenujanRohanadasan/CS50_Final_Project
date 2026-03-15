d466f7170f169952ea6cf099f7e36d42ee06cbc1
Henujan <henujan2k6@gmail.com>
2026-03-15 17:20:22 +0530

    added logout function

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 import os
 
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, redirect
 
 app = Flask(__name__)
 app.secret_key = str(os.environ.get('SECRET_KEY'))
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
+
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def register(): 
     return render_template('register.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/logout')
+def logout():
+    return redirect('/')
